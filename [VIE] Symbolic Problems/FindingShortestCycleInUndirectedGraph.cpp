f2a943273f68a0ff8bbe0ca07cdb0ce15403b65e
Tan Bao Nguyen <jerry.stone.manutd@gmail.com>
2020-03-16 23:22:01 +0800

    Add SP - FindingShortestCycleInUndirectedGraph

--- [VIE] Symbolic Problems/FindingShortestCycleInUndirectedGraph.cpp
+++ [VIE] Symbolic Problems/FindingShortestCycleInUndirectedGraph.cpp
@@ -1,8 +1,27 @@
 /*
-    Tác giả: Nguyễn Tấn Bảo
-    Statement:
-    Idea: 
-
+    Đề bài:
+        - Cho một đồ thị vô hướng, hãy tìm chu trình đơn (simple cycle) ngắn nhất.
+    Lời giải:
+        - Đầu tiên lưu list các cạnh trong mảng "edges", gọi edgesFromNode[x] lưu chỉ số của cạnh có
+        1 trong 2 endpoints là node x trong mảng "edges"
+        - Xét tất cả các đỉnh trong đồ thi, xuất phát từ một đỉnh root nào đó. Tiến hành BFS từ root.
+        - Đầu tiên, với đỉnh root, ta không set distance cho nó mà set distance cho các đỉnh kề với nó
+        trước để việc quay về root trở nên thuận tiện hơn. Lưu chỉ số của cạnh kề root và chỉ số của 
+        đỉnh kề trong edgesFromNode[root] vào làm 1 pair trong queue.
+        - Khi BFS, từ queue ta lấy được 1 node, xét tất cả cạnh kề của node đó trong edgesFromNode[node],
+        chú ý tránh cạnh được dùng để đi đến node (là cạnh ở trong queue vừa lấy ra), khi lấy được 1 cạnh
+        mới, ta lấy node mới và pair để cho vào queue. 
+        - Ở đây ta sẽ có 2 trường hợp:
+            + Đi đến 1 node chưa thăm và node đó không phải root, ở đây đơn giản ta chỉ cần thăm node mới đó.
+            + Đi đến 1 node thăm rồi hoặc node đó là root, vậy chỉ cần tối ưu result bằng cách lấy min của
+            nó với dist[node] + dist[newNode] + 1
+        - Lưu ý là việc tối ưu như trên có thể không chính xác với 1 số root, chẳng hạn như là khi ta
+        đi từ root một hồi xong về lại 1 đỉnh newNode chính trên đường đi từ root đến node, tuy nhiên
+        trường hợp này có thể được cover bằng cách xét 1 root khác trong chu trình đó.
+        - Độ phức tạp O(n^2)
+    Nguồn: 
+        - https://codeforces.com/contest/1325/problem/E
+        - https://codeforces.com/blog/entry/74235
 */
 
 #include <bits/stdc++.h>
@@ -33,65 +52,32 @@ const int MAXM = 1000010;
 const int MAXK = 16;
 const int MAXQ = 200010;
 
-int n;
-int nt[MAXM], dist[MAXM];
-vector<int> d[MAXM], edgesFromNode[MAXM], primes;
+int n, m, dist[MAXN];
+vector<int> edgesFromNode[MAXN];
 vector<vector<int> > edges;
-
-void sieveBase(int N) {
-    // add 1 as a prime
-    primes.push_back(1);
-
-    // d[i] contains list of prime factors of i, remove factors that have 2 occurences
-    FOR(i,2,N) {
-        if (nt[i] == 0) {
-            primes.push_back(i);
-            nt[i] = i;
-            for (ll j = (ll) i * i; j <= N; j += i) nt[j] = i;
-        }
-        d[i] = d[i / nt[i]];
-        vector<int>::iterator it = find(d[i].begin(), d[i].end(), nt[i]);
-        if (it != d[i].end()) d[i].erase(it);
-        else d[i].push_back(nt[i]);
-    }
-}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(nullptr);
-    sieveBase(1000000);
 
-    cin >> n;
-    FOR(i,0,n-1) {
-        int x;
-        cin >> x;
-        if (SZ(d[x]) == 0) {
-            cout << 1;
-            return 0;
-        }
-        // add 1 if there is only one factor in d[x]
-        if (SZ(d[x]) == 1) d[x].push_back(1);
+    cin >> n >> m;
+    FOR(i,0,m-1) {
+        int u, v;
+        cin >> u >> v;
+        edges.push_back({u, v});
 
-        // edges stores list of edges in the graph
-        edges.push_back({d[x][0], d[x][1]});
-
-        // edgesFromNode[i] stores position in "edges" of edges that has i as an endpoint
-        edgesFromNode[d[x][0]].push_back(i);
-        edgesFromNode[d[x][1]].push_back(i);
+        edgesFromNode[u].push_back(i);
+        edgesFromNode[v].push_back(i);
     }
 
     int res = 1000000000;
-    FOR(i,0,SZ(primes)-1) {
-        // consider all primes that are smaller than 1000
-        int pr = primes[i];
-        if (pr * pr > 1000000) break;
-        FOR(j,0,SZ(primes)-1) dist[primes[j]] = 0;
+    FOR(i,1,n) {
+        FOR(j,1,n) dist[j] = 0;
         queue<II> q;
 
-        // mark dist from other endpoints of edges going from "pr"
-        FOR(j,0,SZ(edgesFromNode[pr])-1) {
-            int e = edgesFromNode[pr][j];
-            if (edges[e][0] != pr) {
+        FOR(j,0,SZ(edgesFromNode[i])-1) {
+            int e = edgesFromNode[i][j];
+            if (edges[e][0] != i) {
                 q.push(II(e, 0));
                 dist[edges[e][0]] = 1;
             }
@@ -106,21 +92,18 @@ int main() {
             int node = edges[r.FI][r.SE];
             FOR(j,0,SZ(edgesFromNode[node])-1) {
                 int e = edgesFromNode[node][j];
-                // not going back
+                // không quay ngược lại
                 if (e != r.FI) {
                     II np;
                     if (edges[e][0] != node) np = II(e, 0);
                     else np = II(e, 1);
                     int newNode = edges[np.FI][np.SE];
-                    if (dist[newNode] == 0 && newNode != pr) {
+
+                    if (dist[newNode] == 0 && newNode != i) {
                         q.push(np);
                         dist[newNode] = dist[node] + 1;
                     }
                     else {
-                        // meet a visited node, simply check res
-                        // note that this result may not correct because it may go back
-                        // to a node (newNode) in the path from "pr" to "node" but
-                        // it does not matter, we would check all primes anyway
                         res = min(res, dist[node] + dist[newNode] + 1);
                     }
                 }
---
+++ [VIE] Symbolic Problems/README.md
@@ -0,0 +1,3 @@
+# [VIE] Symbolic Problems
+Đây là danh sách các bài toán mà mình cho là đáng để lưu ý khi thi Competitive Programming, tất cả đều có giải thích bằng tiếng Việt.
+--- farmerboy ---
