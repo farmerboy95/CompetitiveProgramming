ef40f60809c1fa5f14a3b5af290b979b9710d1dc
farmerboy95 <jerry.stone.manutd@gmail.com>
2020-04-23 23:27:06 +0800

    Add CODECHEF FLOW016 AC code

---
+++ Codechef/CODECHEF FLOW016.cpp
@@ -0,0 +1,55 @@
+/*
+    Author: Nguyen Tan Bao
+    Status: AC
+    Idea: GCD and LCM of 2 numbers
+*/
+ 
+#include <bits/stdc++.h>
+#define FI first
+#define SE second
+#define EPS 1e-9
+#define ALL(a) a.begin(),a.end()
+#define SZ(a) int((a).size())
+#define MS(s, n) memset(s, n, sizeof(s))
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define FORE(i,a,b) for (int i = (a); i >= (b); i--)
+#define FORALL(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
+//__builtin_ffs(x) return 1 + index of least significant 1-bit of x
+//__builtin_clz(x) return number of leading zeros of x
+//__builtin_ctz(x) return number of trailing zeros of x
+ 
+using namespace std;
+using ll = long long;
+using ld = double;
+typedef pair<int, int> II;
+typedef pair<II, int> III;
+typedef complex<ld> cd;
+typedef vector<cd> vcd;
+ 
+const ll MODBASE = 1000000007LL;
+const int MAXN = 110;
+const int MAXM = 1000;
+const int MAXK = 16;
+const int MAXQ = 200010;
+
+int gcd(int a, int b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    int t, a, b;
+    cin >> t;
+    while (t--) {
+        cin >> a >> b;
+        int g = gcd(a, b);
+        ll l = (ll) a * b / g;
+        cout << g << ' ' << l << "\n";
+    }
+    return 0;
+}
