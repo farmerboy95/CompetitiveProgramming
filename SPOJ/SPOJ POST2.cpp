a303e5282444145e1edfd74e2c15c43b0a468974
farmerboy95 <jerry.stone.manutd@gmail.com>
2020-11-23 14:37:55 +0800

    Update SPOJ POST2 AC code

--- SPOJ/SPOJ POST2.cpp
+++ SPOJ/SPOJ POST2.cpp
@@ -5,8 +5,7 @@
         - Create polynomial A with A[i] = number of i in the input A
         - Create polynomial B with B[i] = number of i in the input B
         - Multiply A and B using FFT. Degree of each polynomial is 100000 (using offset)
-        - As the degree of the result is 200000, remember to extend the degree of A and B to 200000
-        - Finally, use the offset to get the result.
+        - As the degree of the result is 200000, remember to use the offset properly to get the result.
 */
 
 #include <bits/stdc++.h>
@@ -32,23 +31,17 @@ typedef complex<ld> cd;
 typedef vector<cd> vcd;
 
 const ll MODBASE = 1000000007LL;
-const int MAXN = 100010;
+const int MAXN = 110;
 const int MAXM = 1000;
 const int MAXK = 16;
 const int MAXQ = 200010;
 
-const ld PI = acos(-1);
-
-int n;
-vcd a, b, fa, fb, fh, h;
-
-// only for sz(a) == 2^x
-vcd fft(const vcd &a) {
+void fft(vcd &a, int invert) {
     int n = SZ(a);
+
     int k = 0;
     while ((1<<k) < n) k++;
     vector<int> rev(n);
-
     // 10110 -> rev = 01101
     rev[0] = 0;
     int high1 = -1;
@@ -56,95 +49,73 @@ vcd fft(const vcd &a) {
         if ((i & (i-1)) == 0) high1++;
         rev[i] = rev[i ^ (1<<high1)];
         rev[i] |= (1 << (k-high1-1));
+        if (i < rev[i]) swap(a[i], a[rev[i]]);
     }
 
-    // roots of unity w(n) ^ k = cos(2pi * k / n) + i * sin(2pi * k / n)
-    vcd roots(n);
-    FOR(i,0,n-1) {
-        ld alpha = 2 * PI * i / n;
-        roots[i] = cd(cos(alpha), sin(alpha));
-    }
+    // roots of unity w(n) ^ k = cos(2pi * k / n) (+/-) i * sin(2pi * k / n)
+    vcd roots(n/2);
+    ld alpha = 2 * acos(-1) / n * (invert ? -1 : 1);
+    FOR(i,0,n/2-1) roots[i] = cd(cos(alpha * i), sin(alpha * i));
 
-    vcd cur(n);
-    FOR(i,0,n-1) cur[i] = a[rev[i]];
-
-    // going from leaves to the root of the tree (deepest depth to the root)
-    for (int len = 1; len < n; len <<= 1) {
-        // next depth
-        vcd ncur(n);
-
-        // rstep = radian step
-        // so for length len, we need len roots of unity, w(len * 2) ^ k, 0 <= k <= len-1
-        // in other words, we consider w on one half of the circle
-        int rstep = SZ(roots) / (len * 2);
-        for (int pdest = 0; pdest < n;) {
-            int p1 = pdest;
-            FOR(i,0,len-1) {
-                cd val = roots[i * rstep] * cur[p1 + len];
-                ncur[pdest] = cur[p1] + val;
-                ncur[pdest + len] = cur[p1] - val;
-                pdest++; p1++;
+    for (int len = 2; len <= n; len <<= 1) {
+        int step = n / len;
+        for (int i = 0; i < n; i += len) {
+            FOR(j,0,len/2-1) {
+                cd u = a[i+j], v = a[i+j+len/2] * roots[j * step];
+                a[i+j] = u+v;
+                a[i+j+len/2] = u-v;
             }
-            pdest += len;
         }
-
-        // move next depth to current depth
-        cur.swap(ncur);
     }
 
-    return cur;
+    if (invert) FOR(i,0,n-1) a[i] /= n;
 }
 
-// only for sz(a) == 2^x
-vcd fftRev(const vcd &a) {
-    vcd res = fft(a);
-    FOR(i,0,SZ(res)-1) res[i] /= SZ(a);
-    reverse(res.begin()+1, res.end());
+vector<ll> multiply(vector<ll> &polyA, vector<ll> &polyB) {
+    vcd fPolyA(ALL(polyA)), fPolyB(ALL(polyB));
+    int n = 1;
+    while (n < SZ(polyA) + SZ(polyB) - 1) n <<= 1;
+    fPolyA.resize(n); fPolyB.resize(n);
+
+    fft(fPolyA, 0); fft(fPolyB, 0);
+    FOR(i,0,n-1) fPolyA[i] *= fPolyB[i];
+
+    fft(fPolyA, 1);
+    n = SZ(polyA) + SZ(polyB) - 1;
+    vector<ll> res(n);
+    FOR(i,0,n-1) res[i] = (ll) round(fPolyA[i].real());
+
     return res;
 }
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(0);
-    cin.tie(NULL);
+    cin.tie(nullptr);
+    vector<ll> a, b;
+    int n;
     cin >> n;
     int N = 100000, offset = N / 2;
-    a.resize(2*N+1, 0);
-    b.resize(2*N+1, 0);
+    a.resize(N+1);
+    b.resize(N+1);
     FOR(i,1,n) {
         int x;
         cin >> x;
-        a[x + offset] += cd(1);
+        a[x + offset]++;
     }
     FOR(i,1,n) {
         int x;
         cin >> x;
-        b[x + offset] += cd(1);
+        b[x + offset]++;
     }
 
-    int k = 0;
-    while ((1<<k) < SZ(a)) k++;
-    while (SZ(a) < (1<<k)) a.push_back(0);
-
-    k = 0;
-    while ((1<<k) < SZ(b)) k++;
-    while (SZ(b) < (1<<k)) b.push_back(0);
-
-    fa = fft(a);
-    fb = fft(b);
-
-    fh.resize(SZ(fa));
-
-    FOR(i,0,SZ(a)-1) fh[i] = fa[i] * fb[i];
-    h = fftRev(fh);
-
-    ll res = 0;
+    vector<ll> res = multiply(a, b);
+    ll kq = 0;
     FOR(i,1,n) {
         int x;
         cin >> x;
         x += N;
-        res += (ll) (h[x].real() + 0.5);
+        kq += res[x];
     }
-    cout << res;
+    cout << kq;
     return 0;
 }
