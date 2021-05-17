c4d2214c659eddedceefb2cd6af8d3bc318d95e4
farmerboy95 <jerry.stone.manutd@gmail.com>
2021-05-17 22:46:09 +0800

    Add AtCoder201-ABC-F AC code

---
+++ AtCoder/AtCoder201-ABC-F.cpp
@@ -0,0 +1,174 @@
+/*
+    Author: Nguyen Tan Bao
+    Status: AC
+    Idea:
+        - https://atcoder.jp/contests/abc201/editorial/1838
+*/
+
+#include <bits/stdc++.h>
+#define FI first
+#define SE second
+#define ALL(a) a.begin(), a.end()
+#define SZ(a) int((a).size())
+#define MS(s, n) memset(s, n, sizeof(s))
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define FORE(i,a,b) for (int i = (a); i >= (b); i--)
+#define FORALL(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
+#define TRAV(x, a) for (auto &x : a)
+
+using namespace std;
+using ll = long long; using ld = double; 
+using pi = pair<int, int>; using pl = pair<ll, ll>; using pd = pair<ld, ld>;
+using cd = complex<ld>; using vcd = vector<cd>;
+
+using vi = vector<int>; using vl = vector<ll>;
+using vd = vector<ld>; using vs = vector<string>;
+using vpi = vector<pi>; using vpl = vector<pl>; using vpd = vector<pd>; // vector<pair>
+
+template<class T> using min_pq = priority_queue<T, vector<T>, greater<T> >;
+template<class T> inline int ckmin(T& a, const T& val) { return val < a ? a = val, 1 : 0; }
+template<class T> inline int ckmax(T& a, const T& val) { return a < val ? a = val, 1 : 0; }
+template<class T> void remDup(vector<T>& v) { sort(ALL(v)); v.erase(unique(ALL(v)), end(v)); }
+
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+ll ceilDiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); } // divide a by b rounded up
+ll floorDiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); } // divide a by b rounded down
+void setPrec(int x) { cout << fixed << setprecision(x); }
+
+// TO_STRING
+#define ts to_string
+string ts(char c) { return string(1, c); }
+string ts(const char* s) { return (string) s; }
+string ts(string s) { return s; }
+string ts(bool b) { return ts((int)b); }
+template<class T> string ts(complex<T> c) { stringstream ss; ss << c; return ss.str(); }
+template<class T> using V = vector<T>;
+string ts(V<bool> v) {string res = "{"; FOR(i,0,SZ(v)-1) res += char('0'+v[i]); res += "}"; return res; }
+template<size_t sz> string ts(bitset<sz> b) { string res = ""; FOR(i,0,SZ(b)-1) res += char('0'+b[i]); return res; }
+template<class T, class U> string ts(pair<T,U> p);
+template<class T> string ts(T v) { // containers with begin(), end()
+    bool fst = 1; string res = "";
+    for (const auto& x: v) { if (!fst) res += " "; fst = 0; res += ts(x); }
+    return res;
+}
+template<class T, class U> string ts(pair<T,U> p) { return "("+ts(p.FI)+", "+ts(p.SE)+")"; }
+
+// OUTPUT
+template<class T> void pr(T x) { cout << ts(x); }
+template<class T, class ...U> void pr(const T& t, const U&... u) { pr(t); pr(u...); }
+void ps() { pr("\n"); } // print w/ spaces
+template<class T, class ...U> void ps(const T& t, const U&... u) { pr(t); if (sizeof...(u)) pr(" "); ps(u...); }
+
+// DEBUG
+void DBG() { cerr << "]" << endl; }
+template<class T, class ...U> void DBG(const T& t, const U&... u) { cerr << ts(t); if (sizeof...(u)) cerr << ", "; DBG(u...); }
+#define dbg(...) cerr << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#define chk(...) if (!(__VA_ARGS__)) cerr << "Line(" << __LINE__ << ") -> function(" \
+        << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\n", exit(0);
+
+const ld PI = acos(-1.0);
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+const ld EPS = 1e-9;
+const ll MODBASE = 1000000007LL;
+const int INF = 0x3f3f3f3f;
+
+const int MAXN = 200010;
+const int MAXM = 1000;
+const int MAXK = 16;
+const int MAXQ = 200010;
+
+struct SegmentTreeLazy {
+    public:
+    int N;
+
+    // 0-indexed
+    SegmentTreeLazy(int n) {
+        t.resize(4*n);
+        lazy.resize(4*n);
+        N = n;
+        FOR(i,0,SZ(t)-1) {
+            t[i] = 1e17;
+            lazy[i] = 0;
+        }
+    }
+
+    ll get(int k, int l, int r, int u, int v) {
+        lazyUpdate(k, l, r);
+        if (l > r || r < u || v < l) return 1e18;
+        if (u <= l && r <= v) return t[k];
+        int m = (l + r) >> 1;
+        ll lef = get(k*2, l, m, u, v);
+        ll rig = get(k*2+1, m+1, r, u, v);
+        return merge(lef, rig);
+    }
+
+    void lazyUpdate(int k, int l, int r) {
+        if (l > r) return;
+        if (lazy[k] == 0) return;
+        t[k] += lazy[k];
+        if (l < r) {
+            lazy[k*2] += lazy[k];
+            lazy[k*2+1] += lazy[k];
+        }
+        lazy[k] = 0;
+    }
+
+    void update(int k, int l, int r, int u, int v, ll val) {
+        lazyUpdate(k, l, r);
+        if (l > r || r < u || v < l) return;
+        if (u <= l && r <= v) {
+            lazy[k] = val;
+            lazyUpdate(k, l, r);
+            return;
+        }
+        int m = (l + r) >> 1;
+        update(k*2, l, m, u, v, val);
+        update(k*2+1, m+1, r, u, v, val);
+        t[k] = merge(t[k*2], t[k*2+1]);
+    }
+
+    private:
+    vector<ll> t;
+    vector<ll> lazy;
+
+    ll merge(ll &a, ll &b) {
+        return min(a, b);
+    }
+};
+
+int n, p[MAXN], a[MAXN], b[MAXN], c[MAXN], pos[MAXN];
+ll L[MAXN], R[MAXN], dp[MAXN];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cin >> n;
+    FOR(i,1,n) cin >> p[i];
+    FOR(i,1,n) cin >> a[i] >> b[i] >> c[i];
+
+    FOR(i,1,n) L[i] = L[i-1] + min(a[i], b[i]);
+    FORE(i,n,1) R[i] = R[i+1] + min(a[i], c[i]);
+
+    FOR(i,1,n) pos[p[i]] = i;
+ 
+    SegmentTreeLazy seg(n+1);
+    seg.update(1,0,seg.N-1,0,0,-1e17);
+
+    FOR(i,1,n) {
+        dp[i] = min(L[i-1], seg.get(1,0,seg.N-1,0,pos[i]-1));
+
+        seg.update(1,0,seg.N-1,0,seg.N-1,a[i]);
+
+        ll x = seg.get(1,0,seg.N-1,pos[i],pos[i]);
+        seg.update(1,0,seg.N-1,pos[i],pos[i],-x+dp[i]);
+    }
+
+    ll res = 1e18;
+    FOR(i,0,n) res = min(res, dp[i] + R[i+1]);
+    cout << res; 
+    return 0;
+}
