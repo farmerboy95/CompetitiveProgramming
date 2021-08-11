39c5e398822389dac6d3404ee4eb1e22ab5d0db8
farmerboy95 <jerry.stone.manutd@gmail.com>
2021-08-11 12:16:57 +0800

    Modify SymProb and add some problems

---
+++ [VIE] Symbolic Problems/NumberOfWaysToDivideAnArrayIntoPartsThatSumOfEachPartDivideItsIndex.cpp
@@ -0,0 +1,177 @@
+/*
+    Keywords: dp, separate an array into parts that sum of each part divide part index
+    Đề bài:
+        - Cho 1 mảng n phần tử, chia mảng thành nhiều mảng con sao cho tổng mỗi phần chia
+        hết cho chỉ số của phần đó (tính từ trái sang phải bắt đầu từ 1). Có bao nhiêu cách
+        chia như vậy?
+    Lời giải:
+        - Gọi dp[i][j] = số cách chia mảng a[1..i] thành j phần thoả mãn đề bài. Khi đó
+            dp[i][j] = tổng dp[k][j-1] với mọi k < i sao cho sum[i] - sum[k] chia hết cho j
+        - Làm cách này sẽ có độ phức tạp O(n^3). Làm thế nào để cải tiến nó thành O(n^2)?
+        - Để ý rằng khi sum[i] - sum[k] chia hết cho j thì sum[i] = sum[k] (mod j).
+        - Gọi mem[j][k] = tổng dp[i][j-1] mà sum[i] % j = k 
+        - Ta thêm vào mảng mem mỗi khi kết thúc duyệt ở 1 i nào đó.
+        - Độ phức tạp O(n^2)
+    Nguồn: 
+        - https://atcoder.jp/contests/abc207/tasks/abc207_e
+        - https://atcoder.jp/contests/abc207/editorial/2161
+*/
+
+#include <bits/stdc++.h>
+#define FI first
+#define SE second
+#define ALL(a) a.begin(), a.end()
+#define SZ(a) int((a).size())
+#define MS(s, n) memset(s, n, sizeof(s))
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define FORE(i,a,b) for (int i = (a); i >= (b); i--)
+#define FORALL(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
+#define TRAV(x, a) for (auto &x : a)
+
+using namespace std;
+using ll = long long; using ld = double; 
+using pi = pair<int, int>; using pl = pair<ll, ll>; using pd = pair<ld, ld>;
+using cd = complex<ld>; using vcd = vector<cd>;
+
+using vi = vector<int>; using vl = vector<ll>;
+using vd = vector<ld>; using vs = vector<string>;
+using vpi = vector<pi>; using vpl = vector<pl>; using vpd = vector<pd>; // vector<pair>
+
+template<class T> using min_pq = priority_queue<T, vector<T>, greater<T> >;
+template<class T> inline int ckmin(T& a, const T& val) { return val < a ? a = val, 1 : 0; }
+template<class T> inline int ckmax(T& a, const T& val) { return a < val ? a = val, 1 : 0; }
+template<class T> void remDup(vector<T>& v) { sort(ALL(v)); v.erase(unique(ALL(v)), end(v)); }
+
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+ll ceilDiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); } // divide a by b rounded up
+ll floorDiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); } // divide a by b rounded down
+void setPrec(int x) { cout << fixed << setprecision(x); }
+
+// TO_STRING
+#define ts to_string
+string ts(char c) { return string(1, c); }
+string ts(const char* s) { return (string) s; }
+string ts(string s) { return s; }
+string ts(bool b) { return ts((int)b); }
+template<class T> string ts(complex<T> c) { stringstream ss; ss << c; return ss.str(); }
+template<class T> using V = vector<T>;
+string ts(V<bool> v) {string res = "{"; FOR(i,0,SZ(v)-1) res += char('0'+v[i]); res += "}"; return res; }
+template<size_t sz> string ts(bitset<sz> b) { string res = ""; FOR(i,0,SZ(b)-1) res += char('0'+b[i]); return res; }
+template<class T, class U> string ts(pair<T,U> p);
+template<class T> string ts(T v) { // containers with begin(), end()
+    bool fst = 1; string res = "";
+    for (const auto& x: v) { if (!fst) res += " "; fst = 0; res += ts(x); }
+    return res;
+}
+template<class T, class U> string ts(pair<T,U> p) { return "("+ts(p.FI)+", "+ts(p.SE)+")"; }
+
+// OUTPUT
+template<class T> void pr(T x) { cout << ts(x); }
+template<class T, class ...U> void pr(const T& t, const U&... u) { pr(t); pr(u...); }
+void ps() { pr("\n"); } // print w/ spaces
+template<class T, class ...U> void ps(const T& t, const U&... u) { pr(t); if (sizeof...(u)) pr(" "); ps(u...); }
+
+// DEBUG
+void DBG() { cerr << "]" << endl; }
+template<class T, class ...U> void DBG(const T& t, const U&... u) { cerr << ts(t); if (sizeof...(u)) cerr << ", "; DBG(u...); }
+#define dbg(...) cerr << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#define chk(...) if (!(__VA_ARGS__)) cerr << "Line(" << __LINE__ << ") -> function(" \
+        << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\n", exit(0);
+
+const ld PI = acos(-1.0);
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+const ld EPS = 1e-9;
+const ll MODBASE = 1000000007LL;
+const int INF = 0x3f3f3f3f;
+
+const int MAXN = 110;
+const int MAXM = 1000;
+const int MAXK = 16;
+const int MAXQ = 200010;
+
+template<int MOD> struct mint {
+    static const int mod = MOD;
+    int v; explicit operator int() const { return v; } // explicit -> don't silently convert to int
+    mint() { v = 0; }
+    mint(ll _v) { v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD);
+        if (v < 0) v += MOD; }
+    friend bool operator==(const mint& a, const mint& b) { 
+        return a.v == b.v; }
+    friend bool operator!=(const mint& a, const mint& b) { 
+        return !(a == b); }
+    friend bool operator<(const mint& a, const mint& b) { 
+        return a.v < b.v; }
+    friend string ts(mint a) { return ts(a.v); }
+
+    mint& operator+=(const mint& m) { 
+        if ((v += m.v) >= MOD) v -= MOD; 
+        return *this; }
+    mint& operator-=(const mint& m) { 
+        if ((v -= m.v) < 0) v += MOD; 
+        return *this; }
+    mint& operator*=(const mint& m) { 
+        v = int((ll) v * m.v % MOD); return *this; }
+    mint& operator/=(const mint& m) { return (*this) *= inv(m); }
+    friend mint pow(mint a, ll p) {
+        mint ans = 1; assert(p >= 0);
+        for (; p; p /= 2, a *= a) if (p&1) ans *= a;
+        return ans; }
+    friend mint inv(const mint& a) { assert(a.v != 0); 
+        return pow(a,MOD-2); }
+        
+    mint operator-() const { return mint(-v); }
+    mint& operator++() { return *this += 1; }
+    mint& operator--() { return *this -= 1; }
+    friend mint operator+(mint a, const mint& b) { return a += b; }
+    friend mint operator-(mint a, const mint& b) { return a -= b; }
+    friend mint operator*(mint a, const mint& b) { return a *= b; }
+    friend mint operator/(mint a, const mint& b) { return a /= b; }
+};
+
+typedef mint<MODBASE> mi;
+typedef vector<mi> vmi;
+typedef pair<mi,mi> pmi;
+typedef vector<pmi> vpmi;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vl a(n+1), sum(n+1);
+    FOR(i,1,n) {
+        cin >> a[i];
+        sum[i] = sum[i-1] + a[i];
+    }
+
+    vector<vector<mi>> dp(n+2, vector<mi> (n+2));
+    vector<vector<mi>> mem(n+2, vector<mi> (n+2));
+
+    dp[0][0] = 1;
+    mem[1][0] = 1;
+
+    FOR(i,1,n) {
+        FOR(j,1,n) dp[i][j] = mem[j][sum[i] % j];
+        
+        if (i < n)
+            FOR(j,2,n) {
+                mem[j][sum[i] % j] += dp[i][j-1];
+            }
+
+        // FOR(j,1,i) {
+        //     FOR(k,0,i-1)
+        //         if ((sum[i] - sum[k]) % j == 0) {
+        //             dp[i][j] += dp[k][j-1];
+        //         }
+        // }
+    }
+
+    mi res = 0;
+    FOR(i,1,n) res += dp[n][i];
+    cout << int(res);
+    return 0;
+}
