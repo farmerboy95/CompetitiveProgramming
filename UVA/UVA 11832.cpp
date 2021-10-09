68200fb63518fa5ef3ab8f8a60f2caa6d4f65f89
farmerboy95 <jerry.stone.manutd@gmail.com>
2021-10-10 01:07:03 +0800

    Add CF11832 AC code

---
+++ UVA/UVA 11832.cpp
@@ -0,0 +1,142 @@
+/*
+    Author: Nguyen Tan Bao
+    Status: AC
+    Idea:
+        - DP Knapsack with tracing result
+*/
+
+#include <bits/stdc++.h>
+#define FI first
+#define SE second
+#define ALL(a) a.begin(), a.end()
+#define SZ(a) int((a).size())
+#define MS(s, n) memset(s, n, sizeof(s))
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define FORE(i,a,b) for (int i = (a); i >= (b); i--)
+#define FORALL(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
+#define TRAV(x, a) for (auto &x : a)
+
+using namespace std;
+using ll = long long; using ld = double; 
+using pi = pair<int, int>; using pl = pair<ll, ll>; using pd = pair<ld, ld>;
+using cd = complex<ld>; using vcd = vector<cd>;
+
+using vi = vector<int>; using vl = vector<ll>;
+using vd = vector<ld>; using vs = vector<string>;
+using vpi = vector<pi>; using vpl = vector<pl>; using vpd = vector<pd>; // vector<pair>
+
+template<class T> using min_pq = priority_queue<T, vector<T>, greater<T> >;
+template<class T> inline int ckmin(T& a, const T& val) { return val < a ? a = val, 1 : 0; }
+template<class T> inline int ckmax(T& a, const T& val) { return a < val ? a = val, 1 : 0; }
+template<class T> void remDup(vector<T>& v) { sort(ALL(v)); v.erase(unique(ALL(v)), end(v)); }
+
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+ll ceilDiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); } // divide a by b rounded up
+ll floorDiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); } // divide a by b rounded down
+void setPrec(int x) { cout << fixed << setprecision(x); }
+
+// TO_STRING
+#define ts to_string
+string ts(char c) { return string(1, c); }
+string ts(const char* s) { return (string) s; }
+string ts(string s) { return s; }
+string ts(bool b) { return ts((int)b); }
+template<class T> string ts(complex<T> c) { stringstream ss; ss << c; return ss.str(); }
+template<class T> using V = vector<T>;
+string ts(V<bool> v) {string res = "{"; FOR(i,0,SZ(v)-1) res += char('0'+v[i]); res += "}"; return res; }
+template<size_t sz> string ts(bitset<sz> b) { string res = ""; FOR(i,0,SZ(b)-1) res += char('0'+b[i]); return res; }
+template<class T, class U> string ts(pair<T,U> p);
+template<class T> string ts(T v) { // containers with begin(), end()
+    bool fst = 1; string res = "";
+    for (const auto& x: v) { if (!fst) res += " "; fst = 0; res += ts(x); }
+    return res;
+}
+template<class T, class U> string ts(pair<T,U> p) { return "("+ts(p.FI)+", "+ts(p.SE)+")"; }
+
+// OUTPUT
+template<class T> void pr(T x) { cout << ts(x); }
+template<class T, class ...U> void pr(const T& t, const U&... u) { pr(t); pr(u...); }
+void ps() { pr("\n"); } // print w/ spaces
+template<class T, class ...U> void ps(const T& t, const U&... u) { pr(t); if (sizeof...(u)) pr(" "); ps(u...); }
+
+// DEBUG
+void DBG() { cerr << "]" << endl; }
+template<class T, class ...U> void DBG(const T& t, const U&... u) { cerr << ts(t); if (sizeof...(u)) cerr << ", "; DBG(u...); }
+#define dbg(...) cerr << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#define chk(...) if (!(__VA_ARGS__)) cerr << "Line(" << __LINE__ << ") -> function(" \
+        << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\n", exit(0);
+
+const ld PI = acos(-1.0);
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+const ld EPS = 1e-9;
+const ll MODBASE = 1000000007LL;
+const int INF = 0x3f3f3f3f;
+
+const int MAXN = 41;
+const int MAXM = 80010;
+const int MAXK = 16;
+const int MAXQ = 200010;
+
+int n, k, a[MAXN];
+int dp[MAXN][MAXM];
+const int offset = 40000;
+
+int solve(int id, int val) {
+    int &res = dp[id][val];
+    if (res != -1) return res;
+    int g = solve(id-1, val-a[id]);
+    int h = solve(id-1, val+a[id]);
+    return res = (g | h);
+}
+
+inline bool isInside(int val) {
+    return 0 <= val && val <= 80000;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    while (cin >> n >> k && n + k != 0) {
+        FOR(i,1,n) cin >> a[i];
+        MS(dp, -1);
+        FOR(i,0,80000) dp[0][i] = 0;
+        dp[0][offset] = 1;
+        if (!solve(n, k + offset)) {
+            cout << "*\n";
+            continue;
+        }
+        int cur = n;
+        vi values = {k + offset};
+        string res = "";
+        while (cur) {
+            vi newValues;
+            // +
+            int okPlus = 0;
+            TRAV(v, values)
+                if (isInside(v-a[cur]) && dp[cur-1][v - a[cur]]) {
+                    newValues.push_back(v-a[cur]);
+                    okPlus = 1;
+                }
+            // -
+            int okMinus = 0;
+            TRAV(v, values)
+                if (isInside(v+a[cur]) && dp[cur-1][v + a[cur]]) {
+                    newValues.push_back(v+a[cur]);
+                    okMinus = 1;
+                }
+            if (okMinus && !okPlus) res += "-";
+            else if (!okMinus && okPlus) res += "+";
+            else res += "?";
+            remDup(newValues);
+            swap(values, newValues);
+            cur--;
+        }
+        reverse(ALL(res));
+        cout << res << "\n";
+    }
+    return 0;
+}
