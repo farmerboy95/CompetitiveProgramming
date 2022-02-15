6eee3a5c63113991bf9802c75592850e9a43ac93
farmerboy95 <jerry.stone.manutd@gmail.com>
2022-02-15 13:38:28 +0800

    Add CF1638-D2-E AC code

---
+++ Codeforces/CF1638-D2-E.cpp
@@ -0,0 +1,166 @@
+/*
+    Author: Nguyen Tan Bao
+    Status: AC
+    Idea:
+        - Create a map to store color at a specific node
+        - Create a BIT to store value of nodes
+        - Use an array colorVal[] to store value of colors
+        - Update by adding value of the old color and remove the value of new color (as we
+        won't must remove the value of new color in that node when we move to new color)
+*/
+
+#include <bits/stdc++.h>
+#define FI first
+#define SE second
+#define ALL(a) a.begin(), a.end()
+#define SZ(a) int((a).size())
+#define MS(s, n) memset(s, n, sizeof(s))
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define FORE(i,a,b) for (int i = (a); i >= (b); i--)
+#define FORALL(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
+#define TRAV(x, a) for (auto &x : a)
+
+using namespace std;
+using ll = long long; using ld = double; 
+using pi = pair<int, int>; using pl = pair<ll, ll>; using pd = pair<ld, ld>;
+using cd = complex<ld>; using vcd = vector<cd>;
+
+using vi = vector<int>; using vl = vector<ll>;
+using vd = vector<ld>; using vs = vector<string>;
+using vpi = vector<pi>; using vpl = vector<pl>; using vpd = vector<pd>; // vector<pair>
+
+template<class T> using min_pq = priority_queue<T, vector<T>, greater<T> >;
+template<class T> inline int ckmin(T& a, const T& val) { return val < a ? a = val, 1 : 0; }
+template<class T> inline int ckmax(T& a, const T& val) { return a < val ? a = val, 1 : 0; }
+template<class T> void remDup(vector<T>& v) { sort(ALL(v)); v.erase(unique(ALL(v)), end(v)); }
+
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+ll ceilDiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); } // divide a by b rounded up
+ll floorDiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); } // divide a by b rounded down
+void setPrec(int x) { cout << fixed << setprecision(x); }
+
+// TO_STRING
+#define ts to_string
+string ts(char c) { return string(1, c); }
+string ts(const char* s) { return (string) s; }
+string ts(string s) { return s; }
+string ts(bool b) { return ts((int)b); }
+template<class T> string ts(complex<T> c) { stringstream ss; ss << c; return ss.str(); }
+template<class T> using V = vector<T>;
+string ts(V<bool> v) {string res = "{"; FOR(i,0,SZ(v)-1) res += char('0'+v[i]); res += "}"; return res; }
+template<size_t sz> string ts(bitset<sz> b) { string res = ""; FOR(i,0,SZ(b)-1) res += char('0'+b[i]); return res; }
+template<class T, class U> string ts(pair<T,U> p);
+template<class T> string ts(T v) { // containers with begin(), end()
+    bool fst = 1; string res = "";
+    for (const auto& x: v) { if (!fst) res += " "; fst = 0; res += ts(x); }
+    return res;
+}
+template<class T, class U> string ts(pair<T,U> p) { return "("+ts(p.FI)+", "+ts(p.SE)+")"; }
+
+// OUTPUT
+template<class T> void pr(T x) { cout << ts(x); }
+template<class T, class ...U> void pr(const T& t, const U&... u) { pr(t); pr(u...); }
+void ps() { pr("\n"); } // print w/ spaces
+template<class T, class ...U> void ps(const T& t, const U&... u) { pr(t); if (sizeof...(u)) pr(" "); ps(u...); }
+
+// DEBUG
+void DBG() { cerr << "]" << endl; }
+template<class T, class ...U> void DBG(const T& t, const U&... u) { cerr << ts(t); if (sizeof...(u)) cerr << ", "; DBG(u...); }
+#define dbg(...) cerr << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#define chk(...) if (!(__VA_ARGS__)) cerr << "Line(" << __LINE__ << ") -> function(" \
+        << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\n", exit(0);
+
+const ld PI = acos(-1.0);
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+const ld EPS = 1e-9;
+const ll MODBASE = 1000000007LL;
+const int INF = 0x3f3f3f3f;
+
+const int MAXN = 1000010;
+const int MAXM = 1000;
+const int MAXK = 16;
+const int MAXQ = 200010;
+
+struct BinaryIndexedTree {
+    public:
+    // 1-indexed
+    BinaryIndexedTree(int n) {
+        N = n;
+        t.resize(N+1);
+        FOR(i,0,N) t[i] = 0;
+    }
+
+    void update(int u, ll val) {
+        while (u <= N) {
+            t[u] += val;
+            u = u + (u & (-u));
+        }
+    }
+ 
+    ll get(int u) {
+        ll res = 0;
+        while (u) {
+            res += t[u];
+            u = u - (u & (-u));
+        }
+        return res;
+    }
+
+    private:
+    vector<ll> t;
+    int N;
+};
+
+int n, q;
+ll colorVal[MAXN];
+map<int, int> ma;
+
+void cut(int x) {
+    if (!ma.count(x)) {
+        auto it = prev(ma.lower_bound(x));
+        ma[x] = it->second;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cin >> n >> q;
+    ma[1] = 1;
+    ma[n+1] = 1;
+    BinaryIndexedTree bit(n+1);
+    while (q--) {
+        string st;
+        int l, r, c, x;
+        cin >> st;
+        if (st[0] == 'C') {
+            cin >> l >> r >> c;
+            r++;
+            cut(l); cut(r);
+            auto it = ma.find(l);
+            while (it->first < r) {
+                int L = it->first, R = next(it)->first;
+                int oldCol = it->second;
+                ll diff = colorVal[oldCol] - colorVal[c];
+                bit.update(L, diff);
+                bit.update(R, -diff);
+                it = ma.erase(it);
+            }
+            ma[l] = c;
+        } else if (st[0] == 'A') {
+            cin >> c >> x;
+            colorVal[c] += x;
+        } else {
+            cin >> x;
+            auto it = prev(ma.upper_bound(x));
+            int col = it->second;
+            ll val = bit.get(x);
+            cout << colorVal[col] + val << "\n";
+        }
+    }
+    return 0;
+}
