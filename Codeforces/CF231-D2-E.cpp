4acb3845399cfb774c65c4c559c4420b9233467f
farmerboy95 <jerry.stone.manutd@gmail.com>
2022-06-25 00:14:01 +0800

    Add CF118-D2-E and CF231-D2-E AC code

---
+++ Codeforces/CF231-D2-E.cpp
@@ -0,0 +1,263 @@
+/*
+    Template Version: 1.0.0 - 20220620
+    Author: Nguyen Tan Bao
+    Status: AC
+    Idea:
+        - https://codeforces.com/blog/entry/68138
+*/
+
+#include <bits/stdc++.h>
+#define FI first
+#define SE second
+#define ALL(a) a.begin(), a.end()
+#define SZ(a) int((a).size())
+#define MS(s, n) memset(s, n, sizeof(s))
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define FORE(i,a,b) for (int i = (a); i >= (b); i--)
+#define FORALL(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
+#define TRAV(x, a) for (auto &x : a)
+
+using namespace std;
+using ll = long long; using ld = double; 
+using pi = pair<int, int>; using pl = pair<ll, ll>; using pd = pair<ld, ld>;
+using cd = complex<ld>; using vcd = vector<cd>;
+
+using vi = vector<int>; using vl = vector<ll>;
+using vd = vector<ld>; using vs = vector<string>;
+using vpi = vector<pi>; using vpl = vector<pl>; using vpd = vector<pd>; // vector<pair>
+
+template<class T> using min_pq = priority_queue<T, vector<T>, greater<T> >;
+template<class T> inline int ckmin(T& a, const T& val) { return val < a ? a = val, 1 : 0; }
+template<class T> inline int ckmax(T& a, const T& val) { return a < val ? a = val, 1 : 0; }
+template<class T> void remDup(vector<T>& v) { sort(ALL(v)); v.erase(unique(ALL(v)), end(v)); }
+
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+ll ceilDiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); } // divide a by b rounded up
+ll floorDiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); } // divide a by b rounded down
+void setPrec(int x) { cout << fixed << setprecision(x); }
+
+// TO_STRING
+#define ts to_string
+string ts(char c) { return string(1, c); }
+string ts(const char* s) { return (string) s; }
+string ts(string s) { return s; }
+string ts(bool b) { return (b ? "true" : "false"); }
+
+template<class T> using V = vector<T>;
+template<class T> string ts(complex<T> c);
+string ts(V<bool> v);
+template<size_t sz> string ts(bitset<sz> b);
+template<class T> string ts(T v);
+template<class T, class U> string ts(pair<T,U> p);
+template<class ...U> string ts(tuple<U...> u);
+
+template<class T> string ts(complex<T> c) { stringstream ss; ss << c; return ss.str(); }
+string ts(V<bool> v) {string res = "{"; FOR(i,0,SZ(v)-1) res += char('0'+v[i]); res += "}"; return res; }
+template<size_t sz> string ts(bitset<sz> b) { string res = ""; FOR(i,0,SZ(b)-1) res += char('0'+b[i]); return res; }
+template<class T> string ts(T v) { // containers with begin(), end()
+    bool fst = 1; string res = "";
+    for (const auto& x: v) { if (!fst) res += " "; fst = 0; res += ts(x); }
+    return res;
+}
+template<class T, class U> string ts(pair<T,U> p) { return "(" + ts(p.FI) + ", " + ts(p.SE) + ")"; }
+template<size_t i, class T> string print_tuple_utils(const T& tup) { if constexpr(i == tuple_size<T>::value) return ")"; else return (i ? ", " : "(") + ts(get<i>(tup)) + print_tuple_utils<i + 1, T>(tup); }
+template<class ...U> string ts(tuple<U...> u) { return print_tuple_utils<0, tuple<U...>>(u); }
+
+// OUTPUT
+template<class T> void pr(T x) { cout << ts(x); }
+template<class T, class ...U> void pr(const T& t, const U&... u) { pr(t); pr(u...); }
+void ps() { pr("\n"); } // print w/ spaces
+template<class T, class ...U> void ps(const T& t, const U&... u) { pr(t); if (sizeof...(u)) pr(" "); ps(u...); }
+
+// DEBUG
+void DBG() { cerr << "]" << endl; }
+template<class T, class ...U> void DBG(const T& t, const U&... u) { cerr << ts(t); if (sizeof...(u)) cerr << ", "; DBG(u...); }
+
+#ifdef LOCAL_DEBUG
+#define CONCAT(x, y) x##y
+#define with_level setw(__db_level * 2) << setfill(' ') << "" << setw(0)
+#define dbg(...) cerr << with_level << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#define chk(...) if (!(__VA_ARGS__)) cerr << setw(__db_level * 2) << setfill(' ') << "" << setw(0) << "Line(" << __LINE__ << ") -> function(" << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\n", exit(0);
+#define db_block() debug_block CONCAT(dbbl, __LINE__)
+int __db_level = 0;
+struct debug_block {
+    debug_block() { cerr << with_level << "{" << endl; ++__db_level; }
+    ~debug_block() { --__db_level; cerr << with_level << "}" << endl; }
+};
+#else
+#define dbg(...) 0
+#define chk(...) 0
+#define db_block() 0
+#endif
+
+const ld PI = acos(-1.0);
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+const ld EPS = 1e-9;
+const ll MODBASE = 1000000007LL;
+const int INF = 0x3f3f3f3f;
+
+const int MAXN = 200010;
+const int MAXM = 1000;
+const int MAXK = 16;
+const int MAXQ = 200010;
+
+template<int MOD> struct mint {
+    static const int mod = MOD;
+    int v; explicit operator int() const { return v; } // explicit -> don't silently convert to int
+    mint() { v = 0; }
+    mint(ll _v) { v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD);
+        if (v < 0) v += MOD; }
+    friend bool operator==(const mint& a, const mint& b) { 
+        return a.v == b.v; }
+    friend bool operator!=(const mint& a, const mint& b) { 
+        return !(a == b); }
+    friend bool operator<(const mint& a, const mint& b) { 
+        return a.v < b.v; }
+    friend string ts(mint a) { return ts(a.v); }
+
+    mint& operator+=(const mint& m) { 
+        if ((v += m.v) >= MOD) v -= MOD; 
+        return *this; }
+    mint& operator-=(const mint& m) { 
+        if ((v -= m.v) < 0) v += MOD; 
+        return *this; }
+    mint& operator*=(const mint& m) { 
+        v = int((ll) v * m.v % MOD); return *this; }
+    mint& operator/=(const mint& m) { return (*this) *= inv(m); }
+    friend mint pow(mint a, ll p) {
+        mint ans = 1; assert(p >= 0);
+        for (; p; p /= 2, a *= a) if (p&1) ans *= a;
+        return ans; }
+    friend mint inv(const mint& a) { assert(a.v != 0); 
+        return pow(a,MOD-2); }
+        
+    mint operator-() const { return mint(-v); }
+    mint& operator++() { return *this += 1; }
+    mint& operator--() { return *this -= 1; }
+    friend mint operator+(mint a, const mint& b) { return a += b; }
+    friend mint operator-(mint a, const mint& b) { return a -= b; }
+    friend mint operator*(mint a, const mint& b) { return a *= b; }
+    friend mint operator/(mint a, const mint& b) { return a /= b; }
+};
+
+typedef mint<MODBASE> mi;
+typedef vector<mi> vmi;
+typedef pair<mi,mi> pmi;
+typedef vector<pmi> vpmi;
+
+int n, m, lvl[MAXN], cycleId[MAXN], goingDown[MAXN], parent[MAXN][20], d[MAXN], cnt[MAXN];
+vpi a[MAXN];
+vi A[MAXN];
+vpi edges;
+mi mu[MAXN];
+
+void dfs(int u, int p) {
+    lvl[u] = lvl[p] + 1;
+    int goingUp = 0;
+    FOR(i,0,SZ(a[u])-1) {
+        int v = a[u][i].FI, idx = a[u][i].SE;
+        if (v == p) continue;
+        if (lvl[v] == 0) {
+            dfs(v, u);
+        } else {
+            if (lvl[v] < lvl[u]) {
+                // going up
+                goingUp = idx;
+            } else if (lvl[v] > lvl[u]) {
+                // going down
+                goingDown[u]++;
+            }
+        }
+    }
+
+    if (goingUp) {
+        cycleId[u] = goingUp;
+    } else {
+        cycleId[u] = u;
+        FOR(i,0,SZ(a[u])-1) {
+            int v = a[u][i].FI, idx = a[u][i].SE;
+            if (v == p) continue;
+            if (cycleId[v] != v && goingDown[v] == 0) {
+                cycleId[u] = cycleId[v];
+            }
+        }
+    }
+}
+
+void dfs2(int u, int p) {
+    cnt[u] += (u > n);
+    for (int v : A[u]) {
+        if (v == p) continue;
+        parent[v][0] = u;
+        d[v] = d[u] + 1;
+        cnt[v] += cnt[u];
+        dfs2(v, u);
+    }
+}
+
+int lca(int u, int v) {
+    if (d[v] < d[u]) FORE(i,18,0) if (d[parent[u][i]] >= d[v]) u = parent[u][i];
+    if (d[u] < d[v]) FORE(i,18,0) if (d[parent[v][i]] >= d[u]) v = parent[v][i];
+    FORE(i,18,0) if (parent[u][i] != parent[v][i]) { u = parent[u][i]; v = parent[v][i]; }
+    while (u != v) { u = parent[u][0]; v = parent[v][0]; }
+    return u;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cin >> n >> m;
+    FOR(i,1,m) {
+        int u, v;
+        cin >> u >> v;
+        edges.push_back({u, v});
+        a[u].push_back({v, n+i});
+        a[v].push_back({u, n+i});
+    }
+
+    // squeeze
+    dfs(1, 0);
+
+    vi nodes;
+    TRAV(x, edges) {
+        int u = cycleId[x.FI], v = cycleId[x.SE];
+        nodes.push_back(u);
+        nodes.push_back(v);
+        if (u == v) continue;
+        A[u].push_back(v);
+        A[v].push_back(u);
+    }
+    remDup(nodes);
+    int root = nodes[0];
+    dbg(root);
+
+    d[root] = 1;
+    dfs2(root, 0);
+    TRAV(i, nodes) dbg(i, cnt[i]);
+
+    FOR(j,1,18)
+        TRAV(i, nodes) parent[i][j] = parent[parent[i][j-1]][j-1];
+
+    mu[0] = 1;
+    FOR(i,1,n) mu[i] = mu[i-1] * 2;
+
+    int q;
+    cin >> q;
+    while (q--) {
+        int u, v;
+        cin >> u >> v;
+        u = cycleId[u]; v = cycleId[v];
+        int l = lca(u, v);
+        dbg(u, v, l);
+        int res = cnt[u] + cnt[v] - 2 * cnt[l];
+        if (l > n) {
+            res++;
+        }
+        cout << int(mu[res]) << "\n";
+    }
+    return 0;
+}
