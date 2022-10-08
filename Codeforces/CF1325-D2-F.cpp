b7fb0ba6c1800d0815505e4bea5979e469e96b59
farmerboy95 <jerry.stone.manutd@gmail.com>
2022-10-08 23:52:56 +0800

    Update CF1325-D2-F AC code

--- Codeforces/CF1325-D2-F.cpp
+++ Codeforces/CF1325-D2-F.cpp
@@ -3,55 +3,131 @@
     Status: AC
     Idea:
         - Our graph is undirected, so there are only 2 kinds of edge: Forward edge and Back edge.
-        - Let's say we are at a node u in the DFS tree, consider a back edge that connects u to a 
-        furthest ancestor of u, called v, if number of nodes from v to u >= ceil(sqrt(n)) => we print 2.
-        - No such node v? That means a node can block at most ceil(sqrt(n))-1 other nodes, the ones
-        connected to it by back edges, just mark them and print 1
+        - Let sq denote ceil(sqrt(n))
+        - Let's take the DFS tree of our graph. Assume you're currently in node u in the DFS. 
+        If u has sq-1 or more back-edges, look at the one that connects u to its furthest 
+        ancestor. It forms a cycle of length at least sq. If u doesn't have that many back-edges, 
+        you can add it to the independent set (if none of its neighbors was added). That way, 
+        if you don't find a cycle, every node only blocks at most sq−1 other nodes, the ones 
+        connected to it by a back-edge, so you'll find an independent set!
         - Mainly from https://codeforces.com/blog/entry/74235
 */
- 
+
 #include <bits/stdc++.h>
 #define FI first
 #define SE second
-#define EPS 1e-9
-#define ALL(a) a.begin(),a.end()
+#define ALL(a) a.begin(), a.end()
 #define SZ(a) int((a).size())
 #define MS(s, n) memset(s, n, sizeof(s))
 #define FOR(i,a,b) for (int i = (a); i <= (b); i++)
 #define FORE(i,a,b) for (int i = (a); i >= (b); i--)
 #define FORALL(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
-//__builtin_ffs(x) return 1 + index of least significant 1-bit of x
-//__builtin_clz(x) return number of leading zeros of x
-//__builtin_ctz(x) return number of trailing zeros of x
- 
+#define TRAV(x, a) for (auto &x : a)
+
 using namespace std;
-using ll = long long;
-using ld = double;
-typedef pair<int, int> II;
-typedef pair<II, int> III;
-typedef complex<ld> cd;
-typedef vector<cd> vcd;
- 
+using ll = long long; using ld = double; 
+using pi = pair<int, int>; using pl = pair<ll, ll>; using pd = pair<ld, ld>;
+using cd = complex<ld>; using vcd = vector<cd>;
+
+using vi = vector<int>; using vl = vector<ll>;
+using vd = vector<ld>; using vs = vector<string>;
+using vpi = vector<pi>; using vpl = vector<pl>; using vpd = vector<pd>; // vector<pair>
+
+template<class T> using min_pq = priority_queue<T, vector<T>, greater<T> >;
+template<class T> inline int ckmin(T& a, const T& val) { return val < a ? a = val, 1 : 0; }
+template<class T> inline int ckmax(T& a, const T& val) { return a < val ? a = val, 1 : 0; }
+template<class T> void remDup(vector<T>& v) { sort(ALL(v)); v.erase(unique(ALL(v)), end(v)); }
+
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+ll ceilDiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); } // divide a by b rounded up
+ll floorDiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); } // divide a by b rounded down
+void setPrec(int x) { cout << fixed << setprecision(x); }
+
+// TO_STRING
+#define ts to_string
+string ts(char c) { return string(1, c); }
+string ts(const char* s) { return (string) s; }
+string ts(string s) { return s; }
+string ts(bool b) { return (b ? "true" : "false"); }
+
+template<class T> using V = vector<T>;
+template<class T> string ts(complex<T> c);
+string ts(V<bool> v);
+template<size_t sz> string ts(bitset<sz> b);
+template<class T> string ts(T v);
+template<class T, class U> string ts(pair<T,U> p);
+template<class ...U> string ts(tuple<U...> u);
+
+template<class T> string ts(complex<T> c) { stringstream ss; ss << c; return ss.str(); }
+string ts(V<bool> v) {string res = "{"; FOR(i,0,SZ(v)-1) res += char('0'+v[i]); res += "}"; return res; }
+template<size_t sz> string ts(bitset<sz> b) { string res = ""; FOR(i,0,SZ(b)-1) res += char('0'+b[i]); return res; }
+template<class T> string ts(T v) { // containers with begin(), end()
+    bool fst = 1; string res = "";
+    for (const auto& x: v) { if (!fst) res += " "; fst = 0; res += ts(x); }
+    return res;
+}
+template<class T, class U> string ts(pair<T,U> p) { return "(" + ts(p.FI) + ", " + ts(p.SE) + ")"; }
+template<size_t i, class T> string print_tuple_utils(const T& tup) { if constexpr(i == tuple_size<T>::value) return ")"; else return (i ? ", " : "(") + ts(get<i>(tup)) + print_tuple_utils<i + 1, T>(tup); }
+template<class ...U> string ts(tuple<U...> u) { return print_tuple_utils<0, tuple<U...>>(u); }
+
+// OUTPUT
+template<class T> void pr(T x) { cout << ts(x); }
+template<class T, class ...U> void pr(const T& t, const U&... u) { pr(t); pr(u...); }
+void ps() { pr("\n"); } // print w/ spaces
+template<class T, class ...U> void ps(const T& t, const U&... u) { pr(t); if (sizeof...(u)) pr(" "); ps(u...); }
+
+// DEBUG
+void DBG() { cerr << "]" << endl; }
+template<class T, class ...U> void DBG(const T& t, const U&... u) { cerr << ts(t); if (sizeof...(u)) cerr << ", "; DBG(u...); }
+
+#ifdef LOCAL_DEBUG
+#define CONCAT(x, y) x##y
+#define with_level setw(__db_level * 2) << setfill(' ') << "" << setw(0)
+#define dbg(...) cerr << with_level << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#define chk(...) if (!(__VA_ARGS__)) cerr << setw(__db_level * 2) << setfill(' ') << "" << setw(0) << "Line(" << __LINE__ << ") -> function(" << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\n", exit(0);
+#define db_block() debug_block CONCAT(dbbl, __LINE__)
+int __db_level = 0;
+struct debug_block {
+    debug_block() { cerr << with_level << "{" << endl; ++__db_level; }
+    ~debug_block() { --__db_level; cerr << with_level << "}" << endl; }
+};
+#else
+#define dbg(...) 0
+#define chk(...) 0
+#define db_block() 0
+#endif
+
+const ld PI = acos(-1.0);
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+const ld EPS = 1e-9;
 const ll MODBASE = 1000000007LL;
+const int INF = 0x3f3f3f3f;
+
 const int MAXN = 100010;
 const int MAXM = 1000;
 const int MAXK = 16;
 const int MAXQ = 200010;
 
-int n, m, depth[MAXN], sq, marked[MAXN];
-vector<int> a[MAXN], s;
+int n, m, sq, depth[MAXN], marked[MAXN];
+vi a[MAXN], s;
 
-void dfs(int u) {
+void dfs(int u, int p) {
     s.push_back(u);
-    depth[u] = SZ(s);
-    FOR(i,0,SZ(a[u])-1) {
-        int v = a[u][i];
-        if (!depth[v]) dfs(v);
-        else if (depth[u] - depth[v] + 1 >= sq) {
-            cout << "2\n";
-            cout << depth[u] - depth[v] + 1 << "\n";
-            FOR(j,depth[v]-1,depth[u]-1) cout << s[j] << " ";
-            exit(0);
+    TRAV(v, a[u]) {
+        if (depth[v] == 0) {
+            depth[v] = depth[u] + 1;
+            dfs(v, u);
+        } else {
+            if (depth[u] - depth[v] + 1 >= sq) {
+                cout << 2 << "\n";
+                cout << depth[u] - depth[v] + 1 << "\n";
+                FOR(j,depth[v]-1,depth[u]-1) cout << s[j] << " ";
+                exit(0);
+            }
         }
     }
     if (!marked[u]) {
@@ -67,14 +143,21 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(nullptr);
     cin >> n >> m;
-    while (sq * sq < n) sq++;
     FOR(i,1,m) {
         int u, v;
         cin >> u >> v;
         a[u].push_back(v);
         a[v].push_back(u);
     }
-    dfs(1);
+
+    sq = 1;
+    while (sq * sq < n) {
+        sq++;
+    }
+
+    depth[1] = 1;
+    dfs(1, 0);
+
     cout << "1\n";
     FOR(i,1,n)
         if (!marked[i] && sq) {
