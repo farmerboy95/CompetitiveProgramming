0be832f73a444bfd198d266e64ed638e5cd80b33
farmerboy95 <jerry.stone.manutd@gmail.com>
2022-11-19 23:14:09 +0800

    Add CF514-D2-D AC code

---
+++ Codeforces/CF514-D2-D.cpp
@@ -0,0 +1,196 @@
+/*
+    Template Version: 1.0.0 - 20220620
+    Author: Nguyen Tan Bao
+    Status: AC
+    Idea:
+        - Binary search the result
+        - For each length, we brute force all the ranges, use RMQ to check if a
+        specific range is acceptable
+*/
+
+#include <bits/stdc++.h>
+#define FI first
+#define SE second
+#define ALL(a) a.begin(), a.end()
+#define SZ(a) int((a).size())
+#define MS(s, n) memset(s, n, sizeof(s))
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define FORE(i,a,b) for (int i = (a); i >= (b); i--)
+#define FORALL(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
+#define TRAV(x, a) for (auto &x : a)
+
+using namespace std;
+using ll = long long; using ld = double; 
+using pi = pair<int, int>; using pl = pair<ll, ll>; using pd = pair<ld, ld>;
+using cd = complex<ld>; using vcd = vector<cd>;
+
+using vi = vector<int>; using vl = vector<ll>;
+using vd = vector<ld>; using vs = vector<string>;
+using vpi = vector<pi>; using vpl = vector<pl>; using vpd = vector<pd>; // vector<pair>
+
+template<class T> using min_pq = priority_queue<T, vector<T>, greater<T> >;
+template<class T> inline int ckmin(T& a, const T& val) { return val < a ? a = val, 1 : 0; }
+template<class T> inline int ckmax(T& a, const T& val) { return a < val ? a = val, 1 : 0; }
+template<class T> void remDup(vector<T>& v) { sort(ALL(v)); v.erase(unique(ALL(v)), end(v)); }
+
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+ll ceilDiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); } // divide a by b rounded up
+ll floorDiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); } // divide a by b rounded down
+void setPrec(int x) { cout << fixed << setprecision(x); }
+
+// TO_STRING
+#define ts to_string
+string ts(char c) { return string(1, c); }
+string ts(const char* s) { return (string) s; }
+string ts(string s) { return s; }
+string ts(bool b) { return (b ? "true" : "false"); }
+
+template<class T> using V = vector<T>;
+template<class T> string ts(complex<T> c);
+string ts(V<bool> v);
+template<size_t sz> string ts(bitset<sz> b);
+template<class T> string ts(T v);
+template<class T, class U> string ts(pair<T,U> p);
+template<class ...U> string ts(tuple<U...> u);
+
+template<class T> string ts(complex<T> c) { stringstream ss; ss << c; return ss.str(); }
+string ts(V<bool> v) {string res = "{"; FOR(i,0,SZ(v)-1) res += char('0'+v[i]); res += "}"; return res; }
+template<size_t sz> string ts(bitset<sz> b) { string res = ""; FOR(i,0,SZ(b)-1) res += char('0'+b[i]); return res; }
+template<class T> string ts(T v) { // containers with begin(), end()
+    bool fst = 1; string res = "";
+    for (const auto& x: v) { if (!fst) res += " "; fst = 0; res += ts(x); }
+    return res;
+}
+template<class T, class U> string ts(pair<T,U> p) { return "(" + ts(p.FI) + ", " + ts(p.SE) + ")"; }
+template<size_t i, class T> string print_tuple_utils(const T& tup) { if constexpr(i == tuple_size<T>::value) return ")"; else return (i ? ", " : "(") + ts(get<i>(tup)) + print_tuple_utils<i + 1, T>(tup); }
+template<class ...U> string ts(tuple<U...> u) { return print_tuple_utils<0, tuple<U...>>(u); }
+
+// OUTPUT
+template<class T> void pr(T x) { cout << ts(x); }
+template<class T, class ...U> void pr(const T& t, const U&... u) { pr(t); pr(u...); }
+void ps() { pr("\n"); } // print w/ spaces
+template<class T, class ...U> void ps(const T& t, const U&... u) { pr(t); if (sizeof...(u)) pr(" "); ps(u...); }
+
+// DEBUG
+void DBG() { cerr << "]" << endl; }
+template<class T, class ...U> void DBG(const T& t, const U&... u) { cerr << ts(t); if (sizeof...(u)) cerr << ", "; DBG(u...); }
+
+#ifdef LOCAL_DEBUG
+#define CONCAT(x, y) x##y
+#define with_level setw(__db_level * 2) << setfill(' ') << "" << setw(0)
+#define dbg(...) cerr << with_level << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#define chk(...) if (!(__VA_ARGS__)) cerr << setw(__db_level * 2) << setfill(' ') << "" << setw(0) << "Line(" << __LINE__ << ") -> function(" << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\n", exit(0);
+#define db_block() debug_block CONCAT(dbbl, __LINE__)
+int __db_level = 0;
+struct debug_block {
+    debug_block() { cerr << with_level << "{" << endl; ++__db_level; }
+    ~debug_block() { --__db_level; cerr << with_level << "}" << endl; }
+};
+#else
+#define dbg(...) 0
+#define chk(...) 0
+#define db_block() 0
+#endif
+
+const ld PI = acos(-1.0);
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+const ld EPS = 1e-9;
+const ll MODBASE = 1000000007LL;
+const int INF = 0x3f3f3f3f;
+
+const int MAXN = 110;
+const int MAXM = 1000;
+const int MAXK = 16;
+const int MAXQ = 200010;
+
+struct RMQ {
+    vector<vi> preComp;
+
+    int log2_floor(unsigned long long i) {
+        return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;
+    }
+
+    RMQ(vi &a) {
+        int n = SZ(a);
+        int k = log2_floor(n);
+        preComp.resize(k+1, vi(n));
+
+        FOR(i,0,n-1) {
+            preComp[0][i] = a[i];
+        }
+
+        FOR(i,1,k) {
+            for (int j = 0; j + (1<<i) - 1 < n; j++) {
+                preComp[i][j] = max(preComp[i-1][j], preComp[i-1][j + (1<<(i-1))]);
+            }
+        }
+    }
+
+    int query(int l, int r) {
+        int lg = log2_floor(r - l + 1);
+        return max(preComp[lg][l], preComp[lg][r - (1<<lg) + 1]);
+    }
+};
+
+int n, m, k;
+vi a[5];
+vector<RMQ> rmqs;
+
+vi check(int len, int k) {
+    if (len == 0) {
+        vi res;
+        FOR(j,0,m-1) res.push_back(0);
+        return res;
+    }
+    FOR(i,0,n-1) {
+        int lef = i, rig = i + len - 1;
+        if (rig > n-1) {
+            break;
+        }
+
+        int cur = 0;
+        vi res;
+        FOR(j,0,m-1) {
+            int Max = rmqs[j].query(lef, rig);
+            res.push_back(Max);
+            cur += Max;
+        }
+
+        if (cur <= k) {
+            return res;
+        }
+    }
+
+    return {};
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cin >> n >> m >> k;
+    FOR(i,0,n-1) {
+        FOR(j,0,m-1) {
+            int x;
+            cin >> x;
+            a[j].push_back(x);
+        }
+    }
+    FOR(j,0,m-1) {
+        rmqs.push_back(RMQ(a[j]));
+    }
+
+    int l = 0, r = n;
+    while (l <= r) {
+        int mid = (l + r) >> 1;
+        if (SZ(check(mid, k)) > 0) l = mid + 1;
+        else r = mid - 1;
+    }
+
+    vi res = check(r, k);
+    FOR(i,0,SZ(res)-1) cout << res[i] << ' ';
+    return 0;
+}
