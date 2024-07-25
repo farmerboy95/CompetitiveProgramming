58602bf9dee1a6ccd4f94ed1dce997ca26a175c4
farmerboy95 <jerry.stone.manutd@gmail.com>
2024-07-25 22:10:19 +0100

    Add AtCoder-DP-P, Q, R AC Code

---
+++ AtCoder/AtCoder-DP-Q.cpp
@@ -0,0 +1,168 @@
+/*
+    Template Version: 1.0.0 - 20220620
+    Author: Nguyen Tan Bao
+    Status: AC
+    Idea:
+        - Let dp[i] be the max sum of beauty we can get in the first i flowers and the last flower
+        we keep is i.
+        - Initially, dp[0] = 0.
+        - For each flower i, we can get dp[i] = max(dp[j]) + a[i] for all j < i and h[j] < h[i].
+        This will take O(n^2).
+        - How do we optimize this? We need to know max of the previous dp[j] and the height is
+        less than h[i]. We can use a binary indexed tree to store the previous dp[j] at position 
+        h[j] (because h[j] is unique), then get max from 1 to h[i]-1.
+        - Time complexity: O(n log n)
+        - Space complexity: O(n)
+*/
+
+#include <bits/stdc++.h>
+#define FI first
+#define SE second
+#define ALL(a) a.begin(), a.end()
+#define SZ(a) int((a).size())
+#define MS(s, n) memset(s, n, sizeof(s))
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define FORE(i,a,b) for (int i = (a); i >= (b); i--)
+#define FORALL(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
+#define TRAV(x, a) for (auto &x : a)
+
+using namespace std;
+using ll = long long; using ld = double; 
+using pi = pair<int, int>; using pl = pair<ll, ll>; using pd = pair<ld, ld>;
+using cd = complex<ld>; using vcd = vector<cd>;
+
+using vi = vector<int>; using vl = vector<ll>;
+using vd = vector<ld>; using vs = vector<string>;
+using vpi = vector<pi>; using vpl = vector<pl>; using vpd = vector<pd>; // vector<pair>
+
+template<class T> using min_pq = priority_queue<T, vector<T>, greater<T> >;
+template<class T> inline int ckmin(T& a, const T& val) { return val < a ? a = val, 1 : 0; }
+template<class T> inline int ckmax(T& a, const T& val) { return a < val ? a = val, 1 : 0; }
+template<class T> void remDup(vector<T>& v) { sort(ALL(v)); v.erase(unique(ALL(v)), end(v)); }
+
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+ll ceilDiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); } // divide a by b rounded up
+ll floorDiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); } // divide a by b rounded down
+void setPrec(int x) { cout << fixed << setprecision(x); }
+
+// TO_STRING
+#define ts to_string
+string ts(char c) { return string(1, c); }
+string ts(const char* s) { return (string) s; }
+string ts(string s) { return s; }
+string ts(bool b) { return (b ? "true" : "false"); }
+
+template<class T> using V = vector<T>;
+template<class T> string ts(complex<T> c);
+string ts(V<bool> v);
+template<size_t sz> string ts(bitset<sz> b);
+template<class T> string ts(T v);
+template<class T, class U> string ts(pair<T,U> p);
+template<class ...U> string ts(tuple<U...> u);
+
+template<class T> string ts(complex<T> c) { stringstream ss; ss << c; return ss.str(); }
+string ts(V<bool> v) {string res = "{"; FOR(i,0,SZ(v)-1) res += char('0'+v[i]); res += "}"; return res; }
+template<size_t sz> string ts(bitset<sz> b) { string res = ""; FOR(i,0,SZ(b)-1) res += char('0'+b[i]); return res; }
+template<class T> string ts(T v) { // containers with begin(), end()
+    bool fst = 1; string res = "";
+    for (const auto& x: v) { if (!fst) res += " "; fst = 0; res += ts(x); }
+    return res;
+}
+template<class T, class U> string ts(pair<T,U> p) { return "(" + ts(p.FI) + ", " + ts(p.SE) + ")"; }
+template<size_t i, class T> string print_tuple_utils(const T& tup) { if constexpr(i == tuple_size<T>::value) return ")"; else return (i ? ", " : "(") + ts(get<i>(tup)) + print_tuple_utils<i + 1, T>(tup); }
+template<class ...U> string ts(tuple<U...> u) { return print_tuple_utils<0, tuple<U...>>(u); }
+
+// OUTPUT
+template<class T> void pr(T x) { cout << ts(x); }
+template<class T, class ...U> void pr(const T& t, const U&... u) { pr(t); pr(u...); }
+void ps() { pr("\n"); } // print w/ spaces
+template<class T, class ...U> void ps(const T& t, const U&... u) { pr(t); if (sizeof...(u)) pr(" "); ps(u...); }
+
+// DEBUG
+void DBG() { cerr << "]" << endl; }
+template<class T, class ...U> void DBG(const T& t, const U&... u) { cerr << ts(t); if (sizeof...(u)) cerr << ", "; DBG(u...); }
+
+#ifdef LOCAL_DEBUG
+#define CONCAT(x, y) x##y
+#define with_level setw(__db_level * 2) << setfill(' ') << "" << setw(0)
+#define dbg(...) cerr << with_level << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#define chk(...) if (!(__VA_ARGS__)) cerr << setw(__db_level * 2) << setfill(' ') << "" << setw(0) << "Line(" << __LINE__ << ") -> function(" << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\n", exit(0);
+#define db_block() debug_block CONCAT(dbbl, __LINE__)
+int __db_level = 0;
+struct debug_block {
+    debug_block() { cerr << with_level << "{" << endl; ++__db_level; }
+    ~debug_block() { --__db_level; cerr << with_level << "}" << endl; }
+};
+#else
+#define dbg(...) 0
+#define chk(...) 0
+#define db_block() 0
+#endif
+
+const ld PI = acos(-1.0);
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+const ld EPS = 1e-9;
+const ll MODBASE = 1000000007LL;
+const int INF = 0x3f3f3f3f;
+
+const int MAXN = 200010;
+const int MAXM = 1000;
+const int MAXK = 16;
+const int MAXQ = 200010;
+
+struct BinaryIndexedTree {
+    public:
+    // 1-indexed
+    BinaryIndexedTree(int n) {
+        N = n;
+        t.resize(N+1);
+        FOR(i,0,N) t[i] = 0;
+    }
+
+    void update(int u, ll val) {
+        while (u <= N) {
+            ckmax(t[u], val);
+            u = u + (u & (-u));
+        }
+    }
+ 
+    ll get(int u) {
+        ll res = 0;
+        while (u) {
+            ckmax(res, t[u]);
+            u = u - (u & (-u));
+        }
+        return res;
+    }
+
+    private:
+    vl t;
+    int N;
+};
+
+int n, h[MAXN], a[MAXN];
+ll dp[MAXN];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cin >> n;
+    FOR(i,1,n) cin >> h[i];
+    FOR(i,1,n) cin >> a[i];
+
+    BinaryIndexedTree bit(n);
+    dp[0] = 0;
+    FOR(i,1,n) {
+        dp[i] = bit.get(h[i]-1) + a[i];
+        bit.update(h[i], dp[i]);
+    }
+
+    ll res = 0;
+    FOR(i,1,n) ckmax(res, dp[i]);
+    cout << res;
+    return 0;
+}
---
+++ AtCoder/AtCoder-DP-R.cpp
@@ -0,0 +1,223 @@
+/*
+    Template Version: 1.0.0 - 20220620
+    Author: Nguyen Tan Bao
+    Status: AC
+    Idea:
+        - Let dp[i][j] be the number of ways to reach node j in i steps.
+        - Initially, dp[0][i] = 1, for all i
+        - dp[i][j] = sum(dp[i-1][k]) for all k that there is an edge from k to j
+                   = sum(dp[i-1][k] * a[k][j]) for all k
+        - This will take O(n^2 * k), too slow.
+        - Notice that we can go from dp[i-1] to dp[i] with some kind of matrix, so we can
+        apply matrix exponentiation to optimize the solution. Our matrix A would be like 
+        A[i][j] = a[j][i] (reverse the edge direction)
+        - So dp[k] = A^k * dp[0]
+        - Time complexity: O(n^3 log k)
+        - Space complexity: O(n^2)
+*/
+
+#include <bits/stdc++.h>
+#define FI first
+#define SE second
+#define ALL(a) a.begin(), a.end()
+#define SZ(a) int((a).size())
+#define MS(s, n) memset(s, n, sizeof(s))
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define FORE(i,a,b) for (int i = (a); i >= (b); i--)
+#define FORALL(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
+#define TRAV(x, a) for (auto &x : a)
+
+using namespace std;
+using ll = long long; using ld = double; 
+using pi = pair<int, int>; using pl = pair<ll, ll>; using pd = pair<ld, ld>;
+using cd = complex<ld>; using vcd = vector<cd>;
+
+using vi = vector<int>; using vl = vector<ll>;
+using vd = vector<ld>; using vs = vector<string>;
+using vpi = vector<pi>; using vpl = vector<pl>; using vpd = vector<pd>; // vector<pair>
+
+template<class T> using min_pq = priority_queue<T, vector<T>, greater<T> >;
+template<class T> inline int ckmin(T& a, const T& val) { return val < a ? a = val, 1 : 0; }
+template<class T> inline int ckmax(T& a, const T& val) { return a < val ? a = val, 1 : 0; }
+template<class T> void remDup(vector<T>& v) { sort(ALL(v)); v.erase(unique(ALL(v)), end(v)); }
+
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+ll ceilDiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); } // divide a by b rounded up
+ll floorDiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); } // divide a by b rounded down
+void setPrec(int x) { cout << fixed << setprecision(x); }
+
+// TO_STRING
+#define ts to_string
+string ts(char c) { return string(1, c); }
+string ts(const char* s) { return (string) s; }
+string ts(string s) { return s; }
+string ts(bool b) { return (b ? "true" : "false"); }
+
+template<class T> using V = vector<T>;
+template<class T> string ts(complex<T> c);
+string ts(V<bool> v);
+template<size_t sz> string ts(bitset<sz> b);
+template<class T> string ts(T v);
+template<class T, class U> string ts(pair<T,U> p);
+template<class ...U> string ts(tuple<U...> u);
+
+template<class T> string ts(complex<T> c) { stringstream ss; ss << c; return ss.str(); }
+string ts(V<bool> v) {string res = "{"; FOR(i,0,SZ(v)-1) res += char('0'+v[i]); res += "}"; return res; }
+template<size_t sz> string ts(bitset<sz> b) { string res = ""; FOR(i,0,SZ(b)-1) res += char('0'+b[i]); return res; }
+template<class T> string ts(T v) { // containers with begin(), end()
+    bool fst = 1; string res = "";
+    for (const auto& x: v) { if (!fst) res += " "; fst = 0; res += ts(x); }
+    return res;
+}
+template<class T, class U> string ts(pair<T,U> p) { return "(" + ts(p.FI) + ", " + ts(p.SE) + ")"; }
+template<size_t i, class T> string print_tuple_utils(const T& tup) { if constexpr(i == tuple_size<T>::value) return ")"; else return (i ? ", " : "(") + ts(get<i>(tup)) + print_tuple_utils<i + 1, T>(tup); }
+template<class ...U> string ts(tuple<U...> u) { return print_tuple_utils<0, tuple<U...>>(u); }
+
+// OUTPUT
+template<class T> void pr(T x) { cout << ts(x); }
+template<class T, class ...U> void pr(const T& t, const U&... u) { pr(t); pr(u...); }
+void ps() { pr("\n"); } // print w/ spaces
+template<class T, class ...U> void ps(const T& t, const U&... u) { pr(t); if (sizeof...(u)) pr(" "); ps(u...); }
+
+// DEBUG
+void DBG() { cerr << "]" << endl; }
+template<class T, class ...U> void DBG(const T& t, const U&... u) { cerr << ts(t); if (sizeof...(u)) cerr << ", "; DBG(u...); }
+
+#ifdef LOCAL_DEBUG
+#define CONCAT(x, y) x##y
+#define with_level setw(__db_level * 2) << setfill(' ') << "" << setw(0)
+#define dbg(...) cerr << with_level << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#define chk(...) if (!(__VA_ARGS__)) cerr << setw(__db_level * 2) << setfill(' ') << "" << setw(0) << "Line(" << __LINE__ << ") -> function(" << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\n", exit(0);
+#define db_block() debug_block CONCAT(dbbl, __LINE__)
+int __db_level = 0;
+struct debug_block {
+    debug_block() { cerr << with_level << "{" << endl; ++__db_level; }
+    ~debug_block() { --__db_level; cerr << with_level << "}" << endl; }
+};
+#else
+#define dbg(...) 0
+#define chk(...) 0
+#define db_block() 0
+#endif
+
+const ld PI = acos(-1.0);
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+const ld EPS = 1e-9;
+const ll MODBASE = 1000000007LL;
+const int INF = 0x3f3f3f3f;
+
+const int MAXN = 50;
+const int MAXM = 1000;
+const int MAXK = 16;
+const int MAXQ = 200010;
+
+template<int MOD> struct mint {
+    static const int mod = MOD;
+    int v; explicit operator int() const { return v; } // explicit -> don't silently convert to int
+    mint() { v = 0; }
+    mint(ll _v) { v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD);
+        if (v < 0) v += MOD; }
+    friend bool operator==(const mint& a, const mint& b) { 
+        return a.v == b.v; }
+    friend bool operator!=(const mint& a, const mint& b) { 
+        return !(a == b); }
+    friend bool operator<(const mint& a, const mint& b) { 
+        return a.v < b.v; }
+    friend string ts(mint a) { return ts(a.v); }
+
+    mint& operator+=(const mint& m) { 
+        if ((v += m.v) >= MOD) v -= MOD; 
+        return *this; }
+    mint& operator-=(const mint& m) { 
+        if ((v -= m.v) < 0) v += MOD; 
+        return *this; }
+    mint& operator*=(const mint& m) { 
+        v = int((ll) v * m.v % MOD); return *this; }
+    mint& operator/=(const mint& m) { return (*this) *= inv(m); }
+    friend mint pow(mint a, ll p) {
+        mint ans = 1; assert(p >= 0);
+        for (; p; p /= 2, a *= a) if (p&1) ans *= a;
+        return ans; }
+    friend mint inv(const mint& a) { assert(a.v != 0); 
+        return pow(a,MOD-2); }
+        
+    mint operator-() const { return mint(-v); }
+    mint& operator++() { return *this += 1; }
+    mint& operator--() { return *this -= 1; }
+    friend mint operator+(mint a, const mint& b) { return a += b; }
+    friend mint operator-(mint a, const mint& b) { return a -= b; }
+    friend mint operator*(mint a, const mint& b) { return a *= b; }
+    friend mint operator/(mint a, const mint& b) { return a /= b; }
+};
+
+typedef mint<MODBASE> mi;
+typedef vector<mi> vmi;
+typedef pair<mi,mi> pmi;
+typedef vector<pmi> vpmi;
+
+struct Matrix {
+    vector<vector<mi> > a;
+    int n;
+    Matrix(int n) {
+        this->n = n;
+        a.clear();
+        vector<mi> b(n, 0);
+        FOR(i,0,n-1) a.push_back(b);
+    }
+    Matrix operator * (Matrix &to) {
+        Matrix ans(n);
+        FOR(i,0,n-1)
+            FOR(j,0,n-1)
+                FOR(k,0,n-1) ans.a[i][j] += a[i][k] * to.a[k][j];
+        return ans;
+    }
+    vector<mi> operator * (vector<mi> to) { 
+        vector<mi> ans(n);
+        FOR(i,0,n-1)
+            FOR(j,0,n-1) ans[i] += a[i][j] * to[j];
+        return ans;
+    }
+};
+
+Matrix exp(Matrix A, ll n, Matrix B) {
+    while (n) {
+        if (n&1) B = B*A;
+        A = A*A;
+        n >>= 1;
+    }
+    return B;
+}
+
+int n, a[MAXN][MAXN];
+ll k;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cin >> n >> k;
+    FOR(i,0,n-1) {
+        FOR(j,0,n-1) {
+            cin >> a[i][j];
+        }
+    }
+
+    vmi dp(n, 1);
+
+    Matrix A(n);
+    FOR(i,0,n-1) FOR(j,0,n-1) A.a[i][j] = a[j][i];
+
+    Matrix B(n);
+    FOR(i,0,n-1) B.a[i][i] = 1;
+
+    B = exp(A, k, B);
+
+    dp = B * dp;
+    mi res = 0;
+    FOR(i,0,n-1) res += dp[i];
+    cout << int(res);
+    return 0;
+}
