96285313460e4f072816d03d98c3babce1808dbe
farmerboy95 <jerry.stone.manutd@gmail.com>
2026-02-20 21:56:42 +0700

    Add CF109-D1-C AC code

---
+++ Codeforces/CF109-D1-C.cpp
@@ -0,0 +1,165 @@
+/*
+    Template Version: 1.0.0 - 20220620
+    Author: Nguyen Tan Bao
+    Status: AC
+    Idea:
+        - We use hashing to represent the neighbors of each vertex. We can use double hash 
+        to avoid collision. Then we just need to count how many pairs of vertices have the 
+        same hash, and also check if there are any adjacent vertices that have the same hash 
+        after removing the edge between them.
+        - For hashing, we can use the sum of 2^neighbor for each vertex. To remove the edge 
+        between u and v, we just need to subtract 2^v from h[u] and subtract 2^u from h[v].
+*/
+
+#include <bits/stdc++.h>
+#define FI first
+#define SE second
+#define ALL(a) a.begin(), a.end()
+#define SZ(a) int((a).size())
+#define MS(s, n) memset(s, n, sizeof(s))
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define FORE(i,a,b) for (int i = (a); i >= (b); i--)
+#define FORALL(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
+#define TRAV(x, a) for (auto &x : a)
+
+using namespace std;
+using ll = long long; using ld = double; 
+using pi = pair<int, int>; using pl = pair<ll, ll>; using pd = pair<ld, ld>;
+using cd = complex<ld>; using vcd = vector<cd>;
+
+using vi = vector<int>; using vl = vector<ll>;
+using vd = vector<ld>; using vs = vector<string>;
+using vpi = vector<pi>; using vpl = vector<pl>; using vpd = vector<pd>; // vector<pair>
+
+template<class T> using min_pq = priority_queue<T, vector<T>, greater<T> >;
+template<class T> inline int ckmin(T& a, const T& val) { return val < a ? a = val, 1 : 0; }
+template<class T> inline int ckmax(T& a, const T& val) { return a < val ? a = val, 1 : 0; }
+template<class T> void remDup(vector<T>& v) { sort(ALL(v)); v.erase(unique(ALL(v)), end(v)); }
+
+ll ceilDiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); } // divide a by b rounded up
+ll floorDiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); } // divide a by b rounded down
+void setPrec(int x) { cout << fixed << setprecision(x); }
+
+// TO_STRING
+#define ts to_string
+string ts(char c) { return string(1, c); }
+string ts(const char* s) { return (string) s; }
+string ts(string s) { return s; }
+string ts(bool b) { return (b ? "true" : "false"); }
+
+template<class T> using V = vector<T>;
+template<class T> string ts(complex<T> c);
+string ts(V<bool> v);
+template<size_t sz> string ts(bitset<sz> b);
+template<class T> string ts(T v);
+template<class T, class U> string ts(pair<T,U> p);
+template<class ...U> string ts(tuple<U...> u);
+
+template<class T> string ts(complex<T> c) { stringstream ss; ss << c; return ss.str(); }
+string ts(V<bool> v) {string res = "{"; FOR(i,0,SZ(v)-1) res += char('0'+v[i]); res += "}"; return res; }
+template<size_t sz> string ts(bitset<sz> b) { string res = ""; FOR(i,0,SZ(b)-1) res += char('0'+b[i]); return res; }
+template<class T> string ts(T v) { // containers with begin(), end()
+    bool fst = 1; string res = "";
+    for (const auto& x: v) { if (!fst) res += " "; fst = 0; res += ts(x); }
+    return res;
+}
+template<class T, class U> string ts(pair<T,U> p) { return "(" + ts(p.FI) + ", " + ts(p.SE) + ")"; }
+template<size_t i, class T> string print_tuple_utils(const T& tup) { if constexpr(i == tuple_size<T>::value) return ")"; else return (i ? ", " : "(") + ts(get<i>(tup)) + print_tuple_utils<i + 1, T>(tup); }
+template<class ...U> string ts(tuple<U...> u) { return print_tuple_utils<0, tuple<U...>>(u); }
+
+// OUTPUT
+template<class T> void pr(T x) { cout << ts(x); }
+template<class T, class ...U> void pr(const T& t, const U&... u) { pr(t); pr(u...); }
+void ps() { pr("\n"); } // print w/ spaces
+template<class T, class ...U> void ps(const T& t, const U&... u) { pr(t); if (sizeof...(u)) pr(" "); ps(u...); }
+
+// DEBUG
+void DBG() { cerr << "]" << endl; }
+template<class T, class ...U> void DBG(const T& t, const U&... u) { cerr << ts(t); if (sizeof...(u)) cerr << ", "; DBG(u...); }
+
+#ifdef LOCAL_DEBUG
+#define CONCAT(x, y) x##y
+#define with_level setw(__db_level * 2) << setfill(' ') << "" << setw(0)
+#define dbg(...) cerr << with_level << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#define chk(...) if (!(__VA_ARGS__)) cerr << setw(__db_level * 2) << setfill(' ') << "" << setw(0) << "Line(" << __LINE__ << ") -> function(" << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\n", exit(0);
+#define db_block() debug_block CONCAT(dbbl, __LINE__)
+int __db_level = 0;
+struct debug_block {
+    debug_block() { cerr << with_level << "{" << endl; ++__db_level; }
+    ~debug_block() { --__db_level; cerr << with_level << "}" << endl; }
+};
+#else
+#define dbg(...) 0
+#define chk(...) 0
+#define db_block() 0
+#endif
+
+const ld PI = acos(-1.0);
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+const ld EPS = 1e-9;
+const ll MODBASE1 = 1000000007LL;
+const ll MODBASE2 = 999999991LL;
+const int INF = 0x3f3f3f3f;
+
+const int MAXN = 1000010;
+const int MAXM = 1000;
+const int MAXK = 16;
+const int MAXQ = 200010;
+
+int n, m;
+ll p1[MAXN], p2[MAXN], h1[MAXN], h2[MAXN];
+vi a[MAXN];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cin >> n >> m;
+    FOR(i,1,m) {
+        int u, v;
+        cin >> u >> v;
+        a[u].push_back(v);
+        a[v].push_back(u);
+    }
+
+    p1[0] = p2[0] = 1;
+    FOR(i,1,n) {
+        p1[i] = p1[i-1] * 2 % MODBASE1;
+        p2[i] = p2[i-1] * 2 % MODBASE2;
+    }
+
+    FOR(i,1,n) {
+        TRAV(x, a[i]) {
+            h1[i] = (h1[i] + p1[x-1]) % MODBASE1;
+            h2[i] = (h2[i] + p2[x-1]) % MODBASE2;
+        }
+    }
+
+    vpl v;
+    FOR(i,1,n) v.emplace_back(h1[i], h2[i]);
+    sort(ALL(v));
+
+    ll res = 0;
+    ll cnt = 1;
+    FOR(i,1,n-1) {
+        if (v[i] == v[i-1]) cnt++;
+        else {
+            res += cnt * (cnt - 1) / 2;
+            cnt = 1;
+        }
+    }
+    res += cnt * (cnt - 1) / 2;
+
+    FOR(i,1,n) {
+        TRAV(v, a[i]) {
+            if (v > i) {
+                ll h1i = (h1[i] - p1[v-1] + MODBASE1) % MODBASE1;
+                ll h2i = (h2[i] - p2[v-1] + MODBASE2) % MODBASE2;
+                ll h1v = (h1[v] - p1[i-1] + MODBASE1) % MODBASE1;
+                ll h2v = (h2[v] - p2[i-1] + MODBASE2) % MODBASE2;
+                if (h1i == h1v && h2i == h2v) res++;
+            }
+        }
+    }
+    cout << res << "\n";
+    return 0;
+}
